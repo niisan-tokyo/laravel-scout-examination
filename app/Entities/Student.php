07ad34bca31aa57f12c39530a47635ef8cfe2487
新倉23 <r-niikura@c-tos.com>
2019-08-13 10:33:57 +0900

    import scout

--- app/Entities/Student.php
+++ app/Entities/Student.php
@@ -3,9 +3,23 @@
 namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Laravel\Scout\Searchable;
 
 class Student extends Model
 {
+
+    use Searchable;
+
+    public function toSearchableArray()
+    {
+        $arr = [
+            $this->first_name,
+            $this->last_name,
+            $this->items->pluck('name')->all()
+        ];
+        return Arr::flatten($arr);
+    }
     
     public function items()
     {
@@ -15,6 +29,9 @@ class Student extends Model
     public function scopeSearch($query, string $search)
     {
         $query->where('first_name', 'like', "%${search}%")
-            ->orWhere('last_name', 'like', "%${search}%");
+            ->orWhere('last_name', 'like', "%${search}%")
+            ->orWhereHas('items', function($builder) use ($search) {
+                $builder->where('name', 'like', "%${search}%");
+            });
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        //$this->call(StudentSeeder::class);
-        //$this->call(ItemSeeder::class);
+        $this->call(StudentSeeder::class);
+        $this->call(ItemSeeder::class);
         $this->call(StudentItemSeeder::class);
     }
 }
